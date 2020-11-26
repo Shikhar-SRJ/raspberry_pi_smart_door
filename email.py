14a329fd1ac6be6e47ae1d8e2bc4b3748dbd80e6
Shikhar-SRJ <ptksuraj@gmail.com>
2020-11-26 19:32:54 +0530

    email func1

--- email.py
+++ email.py
@@ -5,62 +5,28 @@ from email.mime.base import MIMEBase
 from email import encoders
 from time import strftime, localtime
 
-t = localtime
-a = strftime("%d_%m_%y", t())
+# fromaddr = "Ali.attendence7422@gmail.com"
+# toaddr = "H00292894@hct.ac.ae"
 
-fromaddr = "Ali.attendence7422@gmail.com"
-toaddr = "H00292894@hct.ac.ae"
-
-# instance of MIMEMultipart
-msg = MIMEMultipart()
-
-# storing the senders email address
-msg['From'] = fromaddr
-
-# storing the receivers email address
-msg['To'] = toaddr
-
-# storing the subject
-msg['Subject'] = "Attendence for today"
-
-# string to store the body of the mail
-body = "Kindly refer to the following attachents."
-
-# attach the body with the msg instance
-msg.attach(MIMEText(body, 'plain'))
-
-# open the file to be sent
-filename = "attendence_" + a + ".csv"
-attachment = open("attendence/"+"attendence_" + a + ".csv", "rb")
-
-# instance of MIMEBase and named as p
-p = MIMEBase('application', 'octet-stream')
-
-# To change the payload into encoded form
-p.set_payload((attachment).read())
-
-# encode into base64
-encoders.encode_base64(p)
-
-p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-# attach the instance 'p' to instance 'msg'
-msg.attach(p)
-
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
-
-# start TLS for security
-s.starttls()
-
-# Authentication
-s.login(fromaddr, "email@2028")
-
-# Converts the Multipart msg into a string
-text = msg.as_string()
-
-# sending the mail
-s.sendmail(fromaddr, toaddr, text)
-
-# terminating the session
-s.quit()
+def send_email(fromaddr = "Ali.attendence7422@gmail.com", toaddr = "H00292894@hct.ac.ae"):
+    t = localtime
+    a = strftime("%d_%m_%y", t())
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Attendence for today"
+    body = "Kindly refer to the following attachents."
+    msg.attach(MIMEText(body, 'plain'))
+    filename = "attendence_" + a + ".csv"
+    attachment = open("attendence/"+"attendence_" + a + ".csv", "rb")
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, "email@2028")
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
